b972790ffc1a170066836941c6486c799bfe027f
Renata <renata.gajzlerowicz@gmail.com>
2019-12-25 10:30:44 +0100

    1

--- spec/features/test2_spec.rb
+++ spec/features/test2_spec.rb
@@ -11,10 +11,13 @@ feature 'Test #2', js:true do
             select 'Opt 2',from:'example_select'
             choose('example_radiobutton_opt_2')
             click_button('Submit')
+
             expect('/pages/test_2')
                 have_css(class:'alert alert-warning')
-                have_content('przyklad')
-                
+                have_content('Submitted text: przyklad')
+                have_content('Checkbox checked: 1')
+                have_content('Select option picked: Opt 2')
+                have_content('Radio option picked: opt2')
         end
         scenario 'Not filling up the form' do
             expect('/pages/test_2')to. have_text('Data will appear here after submission.')
