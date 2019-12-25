f2e8ddefe1dd931590d19055938f54675dddadb3
Renata <renata.gajzlerowicz@gmail.com>
2019-12-25 12:53:50 +0100

    Create test3_spec.rb

---
+++ spec/features/test3_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'Test #3', js:true do
+
+    context 'Checking the steps of Test #3' do
+        scenario 'Highlighting of a table row' do
+            visit '/pages/test_3'
+            within_table id:'test-3-table' do
+                find_field(first('table tr')).click
+            end
+            expect 'first #(table tr)'
+                have_css'highlighted', type: :visible
+        end
+    end
+end
